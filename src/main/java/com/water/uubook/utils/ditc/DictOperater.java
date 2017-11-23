fae509946d20ef46f572ff61e09f61b380e22294
Zhang miaojie <136218949@qq.com>
2017-11-23 18:13:00 +0800

    对数据库表重命名，同时修改dao层

--- src/main/java/com/water/uubook/utils/ditc/DictOperater.java
+++ src/main/java/com/water/uubook/utils/ditc/DictOperater.java
@@ -1,5 +1,7 @@
 package com.water.uubook.utils.ditc;
 
+import java.util.Map;
+
 /**
  * Created by admin on 2017/11/23.
  */
@@ -11,5 +13,9 @@ public class DictOperater {
 
     public static void main(String[] args) {
         System.out.println(getDictionaryDesc("CUSTOMIZE_RULE", "1"));
+    }
+
+    public static Map<String, String> getDictMap(String type) {
+        return DictionaryMap.getDictItemMap(type);
     }
 }
